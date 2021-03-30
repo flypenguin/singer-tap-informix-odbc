b2bf19cbc2600c0d69f0710ea9880dc536c2b4d0
Axel Bock <mr.axel.bock@gmail.com>
2021-03-30 14:14:53 +0200

    Initial commit after cookiecutter

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="singer-tap-informix-odbc",
+    version="0.1.0",
+    description="Singer.io tap for extracting data",
+    author="Axel Bock",
+    url="http://flypenguin.de",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    py_modules=["singer_tap_informix_odbc"],
+    install_requires=[
+        # NB: Pin these to a more specific version for tap reliability
+        "singer-python",
+        "requests",
+    ],
+    entry_points="""
+    [console_scripts]
+    singer-tap-informix-odbc=singer_tap_informix_odbc:main
+    """,
+    packages=["singer_tap_informix_odbc"],
+    package_data={"schemas": ["singer_tap_informix_odbc/schemas/*.json"]},
+    include_package_data=True,
+)
